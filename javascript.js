05868f212b99a163bf91364e19b3d9e8261d1ea3
Chi <richardmlfns@gmail.com>
2024-07-19 17:12:04 +0800

    MVP

---
+++ javascript.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const scroll = new LocomotiveScroll({
+        el: document.querySelector('[data-scroll-container]'),
+        smooth: true
+    });
+});
